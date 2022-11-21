81385e6c4c550793b180b29549c9659b290c4e67
vlad moryakov <sat1sfy@vk.com>
2022-11-21 18:37:19 +0300

    fixes

--- task_manager/users/forms.py
+++ task_manager/users/forms.py
@@ -1,8 +1,13 @@
 from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from django.contrib.auth.models import User
-
+from django import forms
+from django.utils.translation import gettext as _
 
 class RegisterForm(UserCreationForm):
+
+    first_name = forms.CharField(max_length=30, required=True, label=_("First name"))
+    last_name = forms.CharField(max_length=30, required=True, label=_("Last name"))
+
     class Meta:
         model = User
         fields = ["first_name", "last_name", "username",
