f87ce5c1c7987e6cbf58ccf85cdb3327b3f92cee
vlad moryakov <sat1sfy@vk.com>
2022-11-21 18:05:40 +0300

    fix locale

--- task_manager/settings.py
+++ task_manager/settings.py
@@ -118,7 +118,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/4.1/topics/i18n/
 
-LANGUAGE_CODE = 'ru-ru'
+LANGUAGE_CODE = 'ru'
 
 LOCALE_PATHS = (
     os.path.join(BASE_DIR, 'locale'),
