f625f8b80f0129869a8997b25cd851cfc1c5a868
Vladislav Moryakov <102415605+sat-brr@users.noreply.github.com>
2023-03-27 22:16:38 +0300

    Update settings.py
    add host

--- task_manager/settings.py
+++ task_manager/settings.py
@@ -39,7 +39,7 @@ SECRET_KEY = os.getenv("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv("DEBUG") == 'True'
 
-ALLOWED_HOSTS = ['webserver', 'localhost', 'task-manager-sat.up.railway.app',
+ALLOWED_HOSTS = ['webserver', 'localhost', 'https://task-manager-y4e9.onrender.com',
                  '127.0.0.1']
 
 CSRF_TRUSTED_ORIGINS = ['https://*.railway.app']
